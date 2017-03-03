38bd1ff3946c2ea7fb874b116df363c8ad538848
Teng-hao Chang <zhusee2@gmail.com>
2017-03-03 16:11:23 +0800

    Add webpack and basic testing entry

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './css/index.scss';
+import { increment } from './test';
+
+console.log('Hello world', increment());
---
+++ src/test.js
@@ -0,0 +1,9 @@
+const counter = 0;
+
+export function increment() {
+    return counter++;
+}
+
+export function decrement() {
+    return counter--;
+}
