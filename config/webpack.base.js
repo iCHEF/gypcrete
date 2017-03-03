87ddf696b2107f920b61fd2204b3371bc93ea498
Teng-hao Chang <zhusee2@gmail.com>
2017-03-03 18:07:21 +0800

    Update webpack to mount bundled JS as a library

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -6,7 +6,8 @@ module.exports = {
 
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, '../dist')
+        path: path.resolve(__dirname, '../dist'),
+        library: 'gypcrete'
     },
 
     module: {
