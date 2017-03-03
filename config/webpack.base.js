e4feeffa083d1ba0ae07dd71420bb0b160a7bb89
Teng-hao Chang <zhusee2@gmail.com>
2017-03-03 17:12:36 +0800

    Setup loaders for sass + postcss

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,10 +1,48 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
 
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, '../lib')
-    }
+        path: path.resolve(__dirname, '../dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: () => [
+                                    require('autoprefixer')
+                                ]
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                outputStyle: 'expanded'
+                            }
+                        }
+                    ]
+                })
+            }
+        ]
+    },
+
+    plugins: [
+        new ExtractTextPlugin('bundle.css')
+    ]
 };
