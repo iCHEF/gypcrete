c2aa5c2032fedca880c76282ea985f12040d5afd
Teng-hao Chang <zhusee2@gmail.com>
2017-03-13 15:30:10 +0800

    Remove 'npm publish' from deploy script; force push tags.
    'npm publish' is removed since it fails all the time as long as it stays in 'deploy.sh' on Jenkins.

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ rm -rf deploy || exit 0
 if [ "$GIT_BRANCH" = "origin/master" ]; then
     export PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
     git tag -a "${PACKAGE_VERSION}" -m "Deployed with ${BUILD_TAG}"
-    git push --tag origin || exit 1
+    git push --tag --force origin || exit 1
 fi
 
 # Git clone
@@ -27,8 +27,3 @@ git commit -m "Built at ${BUILD_TAG}" --author="ichefbot <developer@ichef.com.tw
 
 # Push to dist branch
 git push origin dist
-
-# Publish to npm (only in master)
-if [ "$GIT_BRANCH" = "origin/master" ]; then
-    npm publish || exit 1
-fi
