d861fc60db28ff89da6b016885407d1b87a33f6d
Teng-hao Chang <zhusee2@gmail.com>
2017-03-16 15:37:40 +0800

    Add shallow render tests for <StatusIcon>

--- src/__tests__/StatusIcon.test.js
+++ src/__tests__/StatusIcon.test.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
+import Icon from '../Icon';
 import StatusIcon from '../StatusIcon';
 
 it('renders without crashing', () => {
@@ -7,4 +10,24 @@ it('renders without crashing', () => {
     const element = <StatusIcon status="loading" />;
 
     ReactDOM.render(element, div);
+});
+
+it('renders nothing if status not recognized', () => {
+    const wrapper = shallow(<StatusIcon status="foo-bar" />);
+    expect(wrapper.children()).toHaveLength(0);
+});
+
+it('renders success icon', () => {
+    const wrapper = shallow(<StatusIcon status="success" />);
+    expect(wrapper.find(Icon).prop('type')).toBe('inline-success');
+});
+
+it('renders loading icon', () => {
+    const wrapper = shallow(<StatusIcon status="loading" />);
+    expect(wrapper.find(Icon).prop('type')).toBe('inline-loading');
+});
+
+it('renders error icon', () => {
+    const wrapper = shallow(<StatusIcon status="error" />);
+    expect(wrapper.find(Icon).prop('type')).toBe('inline-error');
 });
