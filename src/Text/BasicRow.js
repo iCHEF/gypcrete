842e0c5b80d3bccb788c45ec9ccfeae86eb068fb
Teng-hao Chang <zhusee2@gmail.com>
2017-03-20 12:14:28 +0800

    Add comments for 'wrapIfNotElement()' inside <BasicRow>

--- src/Text/BasicRow.js
+++ src/Text/BasicRow.js
@@ -4,6 +4,15 @@ import FlexCell from '../FlexCell';
 import Tag from '../Tag';
 import TextEllipsis from '../TextEllipsis';
 
+/**
+ * Wrap the passed-in `content` with a component if it's not a React element,
+ * to make sure the result will always be a HTML tag.
+ *
+ * @param {Any} content
+ * @param {Component} Wrapper
+ *
+ * @return {Element}
+ */
 function wrapIfNotElement(content, { with: Wrapper }) {
     if (React.isValidElement(content)) {
         return content;
