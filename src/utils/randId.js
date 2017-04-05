43a4ecb03e16e98ff8dfe24d3770f82c0d1decf6
Teng-hao Chang <zhusee2@gmail.com>
2017-04-05 15:24:37 +0800

    Migrate randId() helper to generate random IDs for DOM nodes

---
+++ src/utils/randId.js
@@ -0,0 +1,13 @@
+/**
+ * Generates a random String at specific length to be used as IDs.
+ *
+ * @param {Number} length - the length of generated String.
+ * @param {String} prefix
+ * @return {String} result
+ */
+function randId({ length = 10, prefix = 'node' } = {}) {
+    const randHash = Math.random().toString(16).substr(2, length);
+    return `${prefix}-${randHash}`;
+}
+
+export default randId;
