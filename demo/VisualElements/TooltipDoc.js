0c66c932c3196e320bd9d717f2e5994a197653d8
Teng-hao Chang <zhusee2@gmail.com>
2017-04-07 18:08:04 +0800

    Remove bad example of anchored tooltip, in favor of the on in <IconButton>.

--- demo/VisualElements/TooltipDoc.js
+++ demo/VisualElements/TooltipDoc.js
@@ -1,39 +1,12 @@
 import React from 'react';
 import Tooltip from 'src/Tooltip';
 
-import Icon from 'src/Icon';
-import anchored from 'src/mixins/anchored';
-
 function TooltipBox({ children }) {
     return (
         <div style={{ display: 'inline-block', padding: '0 1em' }}>
             {children}
         </div>
     );
-}
-
-const AnchoredTooltip = anchored({ defaultPlacement: 'top', padding: 0 })(Tooltip);
-
-class FooBox extends React.PureComponent {
-    state = {
-        show: false
-    }
-
-    componentDidMount() {
-        setTimeout(() => this.setState({ show: true }), 500);
-    }
-
-    render() {
-        return (
-            <div style={{ marginTop: 60 }}>
-                <span ref={(r) => { this.spanRef = r; }}>
-                    <Icon type="printer" />
-                </span>
-
-                {this.state.show && <AnchoredTooltip anchor={this.spanRef} />}
-            </div>
-        );
-    }
 }
 
 function TooltipDoc() {
@@ -48,8 +21,6 @@ function TooltipDoc() {
             <TooltipBox>
                 <Tooltip placement="bottom">Tooltip</Tooltip>
             </TooltipBox>
-
-            <FooBox />
         </div>
     );
 }
