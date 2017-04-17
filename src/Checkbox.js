222b0a67a546f54636e757fff29b4bbbfd858567
Teng-hao Chang <zhusee2@gmail.com>
2017-04-17 14:08:05 +0800

    Fix `indeterminate` prop should not be passed to <div>

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -74,6 +74,7 @@ class Checkbox extends PureComponent {
     render() {
         const {
             input,
+            indeterminate,
             // <input> props
             checked,
             defaultChecked,
