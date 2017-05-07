aac75f0f208e7511059fb7b8e95ebda1bee73839
CJ <cijies@gmail.com>
2017-05-08 00:11:02 +0800

    [demo] Remove `demo/` folder

--- examples/Icon/index.js
+++ examples/Icon/index.js
@@ -7,6 +7,8 @@ import Icon from 'src/Icon';
 import BasicIconsSet from './BasicIcons';
 import PaymentIconsSet from './PaymentIcons';
 import CRMIconsSet from './CRMIcons';
+import InventoryIconsSet from './InventoryIcons';
+
 import LargeIconExample from './LargeIcon';
 import IconColorsExample from './IconColors';
 
@@ -14,6 +16,7 @@ storiesOf('Icon', module)
     .addWithInfo('basic icons set', BasicIconsSet)
     .addWithInfo('payment icons set', PaymentIconsSet)
     .addWithInfo('CRM icons set', CRMIconsSet)
+    .addWithInfo('Inventory icons set', InventoryIconsSet)
     .addWithInfo('large size', LargeIconExample)
     .addWithInfo('color options', IconColorsExample)
     // Props table
