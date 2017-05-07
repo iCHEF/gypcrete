0cc98025598633c5ff541225e641b05a516209fc
CJ <cijies@gmail.com>
2017-05-07 23:47:46 +0800

    [examples] Migrate <Tooltip> example

---
+++ examples/Tooltip/BasicTooltip.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Tooltip from 'src/Tooltip';
+
+function BasicTooltipExample() {
+    return (
+        <Tooltip>tooltip</Tooltip>
+    );
+}
+
+export default BasicTooltipExample;
