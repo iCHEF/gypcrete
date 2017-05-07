56db98ced5cd1231e38765d7ef36b8d6039479b0
CJ <cijies@gmail.com>
2017-05-07 14:19:21 +0800

    [storybook] Install addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,27 @@
-import { configure } from '@kadira/storybook';
+import { configure, setAddon } from '@kadira/storybook';
+import { setOptions } from '@kadira/storybook-addon-options';
+import infoAddon, { setDefaults } from '@kadira/react-storybook-addon-info';
+
+// -------------------------------------
+//   Addons
+// -------------------------------------
+
+setOptions({
+  name: 'iCHEF gypcrete',
+  url: 'https://github.com/iCHEF/gypcrete',
+  showDownPanel: false
+});
+
+setDefaults({
+    inline: true,
+    // source: false,
+    propTables: false
+});
+setAddon(infoAddon);
+
+// -------------------------------------
+//   Load Stories
+// -------------------------------------
 
 const reqContext = require.context(
     '../examples/',
