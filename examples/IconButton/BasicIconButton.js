a42e86c58fdae2aaca1937c7bdf4fe2812ac5435
CJ <cijies@gmail.com>
2017-05-16 14:04:32 +0800

    [examples] Fix basic usage of <IconButton>

--- examples/IconButton/BasicIconButton.js
+++ examples/IconButton/BasicIconButton.js
@@ -1,19 +1,10 @@
 import React from 'react';
 
 import IconButton from 'src/IconButton';
-import FlexRow from '../FlexRow';
 
 function BasicIconButtonExample() {
     return (
-        <FlexRow>
-            <IconButton
-                icon="printer" />
-
-            <IconButton
-                solid
-                color="blue"
-                icon="edit" />
-        </FlexRow>
+        <IconButton icon="printer" />
     );
 }
 
