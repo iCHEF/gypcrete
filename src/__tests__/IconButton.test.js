f7a11af3859d89a601089919a5d69f851f92354e
CJ <cijies@gmail.com>
2017-05-16 13:51:49 +0800

    [test] Test invalid propTypes of <IconButton>

--- src/__tests__/IconButton.test.js
+++ src/__tests__/IconButton.test.js
@@ -1,30 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
 import Button from '../Button';
 import IconButton from '../IconButton';
 import IconLayout from '../IconLayout';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    const element = <IconButton icon="printer" status="loading" />;
+describe('<IconButton>', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        const element = <IconButton icon="printer" status="loading" />;
 
-    ReactDOM.render(element, div);
-});
+        ReactDOM.render(element, div);
+    });
 
-it('renders as a custom-configured variant of <Button>', () => {
-    const wrapper = shallow(<IconButton icon="printer" status="loading" />);
+    it('renders as a custom-configured variant of <Button>', () => {
+        const wrapper = shallow(<IconButton icon="printer" status="loading" />);
 
-    expect(wrapper.children()).toHaveLength(1);
-    expect(wrapper.find(Button).exists()).toBeTruthy();
-    expect(wrapper.find(Button).prop('status')).toBe('loading');
-});
+        expect(wrapper.children()).toHaveLength(1);
+        expect(wrapper.find(Button).exists()).toBeTruthy();
+        expect(wrapper.find(Button).prop('status')).toBe('loading');
+    });
 
-it('renders icon into <IconLayout>', () => {
-    const wrapper = shallow(<IconButton icon="printer" />);
-    const buttonChidlren = wrapper.find(Button).children();
+    it('renders icon into <IconLayout>', () => {
+        const wrapper = shallow(<IconButton icon="printer" />);
+        const buttonChidlren = wrapper.find(Button).children();
 
-    expect(buttonChidlren).toHaveLength(1);
-    expect(buttonChidlren.equals(<IconLayout icon="printer" />)).toBeTruthy();
+        expect(buttonChidlren).toHaveLength(1);
+        expect(buttonChidlren.equals(<IconLayout icon="printer" />)).toBeTruthy();
+    });
+
+    describe('invalid propTypes', () => {
+        let consoleStub;
+        beforeEach(() => {
+            consoleStub = sinon.stub(console, 'error');
+        });
+
+        afterEach(() => {
+            consoleStub.restore();
+        });
+
+        it('color must be empty', () => {
+            shallow(<IconButton icon="printer" color="red" />);
+            sinon.assert.calledWithMatch(consoleStub, /Failed prop type: <IconButton> must not contains color./);
+        });
+
+        it('solid must be empty', () => {
+            shallow(<IconButton icon="printer" solid />);
+            sinon.assert.calledWithMatch(consoleStub, /Failed prop type: <IconButton> must not contains solid./);
+        });
+    });
 });
