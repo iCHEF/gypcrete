2cefb8d0facdf22dc68be414ca37cbd9ad2d83aa
Teng-hao Chang <zhusee2@gmail.com>
2017-05-18 19:24:56 +0800

    Update interactive examples for <EditableTextLabel>

--- examples/TextLabel/Editable.js
+++ examples/TextLabel/Editable.js
@@ -1,39 +1,58 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { action } from '@kadira/storybook';
 
 import EditableTextLabel from 'src/EditableTextLabel';
 import DebugBox from '../DebugBox';
 
+class EditableExample extends PureComponent {
+    state = {
+        isEditing: false,
+        currentBasic: 'Kitchen Printer',
+    };
+
+    handleEditRequest = (event) => {
+        this.setState({ isEditing: true });
+        action('editRequest')(event);
+    }
+
+    handleEditEnd = (event) => {
+        if (!event.reset) {
+            this.setState({ currentBasic: event.value });
+        }
+
+        this.setState({ isEditing: false });
+        action('editEnd')(event);
+    }
+
+    render() {
+        return (
+            <DebugBox>
+                <EditableTextLabel
+                    inEdit={this.state.isEditing}
+                    icon="printer"
+                    basic={this.state.currentBasic}
+                    aside="00:11:22:33"
+                    tag="Online"
+                    onEditRequest={this.handleEditRequest}
+                    onEditEnd={this.handleEditEnd} />
+            </DebugBox>
+        );
+    }
+}
+
 function Editable() {
     return (
         <div>
-            <DebugBox>
-                <EditableTextLabel
-                    icon="printer"
-                    basic="Kitchen Printer A"
-                    aside="00:11:22:33"
-                    tag="Online"
-                    onEditRequest={action('editRequest')} />
-            </DebugBox>
+            <EditableTextLabel
+                icon="printer"
+                basic="Kitchen Printer"
+                aside="00:11:22:33"
+                tag="Online"
+                onEditRequest={action('editRequest')}
+                onEditEnd={action('editEnd')} />
 
-            <DebugBox>
-                <EditableTextLabel
-                    inEdit
-                    icon="printer"
-                    basic="Kitchen Printer A"
-                    aside="00:11:22:33"
-                    tag="Online"
-                    onEditEnd={action('editEnd')} />
-            </DebugBox>
-
-            <DebugBox>
-                <EditableTextLabel
-                    status="loading"
-                    icon="printer"
-                    basic="Kitchen Printer A"
-                    aside="00:11:22:33"
-                    tag="Online" />
-            </DebugBox>
+            <p>Interactive example:</p>
+            <EditableExample />
         </div>
     );
 }
