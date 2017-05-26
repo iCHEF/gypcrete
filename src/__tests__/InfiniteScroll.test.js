4dcfcdeecc9078be46d52ced77d2cc8f1487b60c
CJ <cijies@gmail.com>
2017-05-26 11:25:01 +0800

    [tests] Update test cases

--- src/__tests__/InfiniteScroll.test.js
+++ src/__tests__/InfiniteScroll.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme';
 
-import InfiniteScroll, { DefaultLoadingSpinner } from '../InfiniteScroll';
+import InfiniteScroll, { BEM } from '../InfiniteScroll';
+import Button from '../Button';
 
+const footerClassName = `.${BEM.footer}`;
 const SCROLL_EVENT = new Event('scroll');
 const FAKE_LIST = (
     <ul>
@@ -25,41 +27,74 @@ describe('InfiniteScroll', () => {
         ReactDOM.render(element, div);
     });
 
-    it('should show loading spinner while loading', () => {
+    // -------------------------------------
+    //   Verify footer Button(s)
+    // -------------------------------------
+
+    it('should show loading button while loading', () => {
         const wrapper = shallow(
             <InfiniteScroll
                 isLoading
                 onInfiniteLoad={() => {}} />
         );
 
-        expect(wrapper.find(DefaultLoadingSpinner).exists()).toBeTruthy();
+        // show loading icon in default
+        expect(wrapper.find(footerClassName).find(Button).prop('icon').props.type).toBe('loading');
+        expect(wrapper.find(footerClassName).find(Button).prop('basic')).toBe(null);
+
+        // loadingButton as string
+        wrapper.setProps({ loadingButton: 'loading...' });
+        expect(wrapper.find(footerClassName).find(Button).prop('basic')).toBe('loading...');
+
+        // loadingButton as element
+        const CustomLoadingButton = () => (<div>Loading...</div>);
+        wrapper.setProps({ loadingButton: <CustomLoadingButton /> });
+        expect(wrapper.find(footerClassName).find(CustomLoadingButton).exists()).toBeTruthy();
     });
 
-    it('should show custom loading spinner', () => {
-        const CustomLoadingSpinner = () => (<div>Loading...</div>);
+    it('should render show more button', () => {
         const wrapper = shallow(
             <InfiniteScroll
-                isLoading
-                loadingSpinner={<CustomLoadingSpinner />}
+                showMoreButton="show more"
                 onInfiniteLoad={() => {}} />
         );
 
-        expect(wrapper.find(CustomLoadingSpinner).exists()).toBeTruthy();
+        // showMoreButton as string
+        expect(wrapper.find(footerClassName).find(Button).prop('basic')).toBe('show more');
+
+        // showMoreButton as element
+        const CustomShowMoreButton = () => (<div>show more</div>);
+        wrapper.setProps({ showMoreButton: <CustomShowMoreButton /> });
+        expect(wrapper.find(footerClassName).find(CustomShowMoreButton).exists()).toBeTruthy();
+
+        // Hide showMoreButton
+        wrapper.setProps({ showMoreButton: undefined });
+        expect(wrapper.find(footerClassName).children().length).toBe(0);
     });
 
-    it('should render endMessage if hasMore prop set as false', () => {
-        const EndMessage = () => (<div>End Message</div>);
+    it('should render no newest button if hasMore set as false', () => {
         const wrapper = shallow(
             <InfiniteScroll
-                endMessage={<EndMessage />}
+                showMoreButton="show more"
+                noNewestButton="all displayed"
                 onInfiniteLoad={() => {}} />
         );
 
-        // hasMore default as true
-        expect(wrapper.find(EndMessage).exists()).toBeFalsy();
+        // Render showMoreButton if hasMore is true
+        expect(wrapper.find(footerClassName).find(Button).prop('basic')).toBe('show more');
 
+        // Turn hasMore to false
         wrapper.setProps({ hasMore: false });
-        expect(wrapper.find(EndMessage).exists()).toBeTruthy();
+        expect(wrapper.find(footerClassName).find(Button).prop('basic')).toBe('all displayed');
+
+        // noNewestButton as element
+        const CustomNoNewestButton = () => (<div>all displayed</div>);
+        wrapper.setProps({ noNewestButton: <CustomNoNewestButton /> });
+        expect(wrapper.find(footerClassName).find(CustomNoNewestButton).exists()).toBeTruthy();
+
+        // Hide noNewestButton
+        wrapper.setProps({ noNewestButton: undefined });
+        expect(wrapper.find(footerClassName).children().length).toBe(0);
     });
 
     // -------------------------------------
@@ -147,6 +182,4 @@ describe('InfiniteScroll', () => {
         global.window.dispatchEvent(SCROLL_EVENT);
         expect(onInfiniteLoad).toHaveBeenCalledTimes(1);
     });
-
-
 });
