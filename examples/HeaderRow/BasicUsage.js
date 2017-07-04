622cd99f58c33b8c34f96c889aec915f074dd4da
Teng-hao Chang <zhusee2@gmail.com>
2017-07-04 16:51:16 +0800

    Make label in header example wrapped with <TextEllipsis>

--- examples/HeaderRow/BasicUsage.js
+++ examples/HeaderRow/BasicUsage.js
@@ -3,16 +3,23 @@ import React from 'react';
 import HeaderRow from 'src/HeaderRow';
 import Button from 'src/Button';
 import TextLabel from 'src/TextLabel';
+import TextEllipsis from 'src/TextEllipsis';
 
 import DebugBox from '../DebugBox';
 
 function BasicUsage() {
     const leftBtn = <Button icon="prev" basic="Back" />;
     const rightBtn = <Button basic="Save" />;
-    const centerLabel = <TextLabel basic="A slightly longer title" />;
+    const centerLabel = (
+        <TextLabel
+            align="center"
+            basic={
+                <TextEllipsis>Lorem ipsum a slightly longer title</TextEllipsis>
+            } />
+    );
 
     return (
-        <DebugBox width="30rem">
+        <DebugBox>
             <HeaderRow
                 left={leftBtn}
                 center={centerLabel}
