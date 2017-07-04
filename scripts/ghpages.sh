9887470814baaf50db0aa9b7f311a09ad2d396d5
CJ <cijies@gmail.com>
2017-07-04 10:22:59 +0800

    [ci] Update deploy & ghpages scripts to fit TravisCI

--- scripts/ghpages.sh
+++ scripts/ghpages.sh
@@ -4,9 +4,13 @@
 rm -rf deploy || exit 0
 
 # Only deploy storybook from master branch
-if [ "$GIT_BRANCH" = "origin/master" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    # Setup git
+    git config --global user.name "ichefbot"
+    git config --global user.email "developer@ichef.com.tw"
+
     # Git clone
-    git clone -b gh-pages --single-branch git@github.com:iCHEF/gypcrete.git deploy
+    git clone -b gh-pages --single-branch "https://${GH_REPO}" deploy
 
     # Clean up files from last build(except .git)
     find ./deploy/* ! -path "./deploy/.git/*" ! -name ".git" | xargs rm -rf
@@ -14,9 +18,12 @@ if [ "$GIT_BRANCH" = "origin/master" ]; then
     # Copy public files
     cp -R ./public/. ./deploy
 
-    # Commit and push
+    # Commit files
     cd deploy
     git add .
-    git commit -m "Built at ${BUILD_TAG}" --author="ichefbot <developer@ichef.com.tw>"
-    git push origin gh-pages
+    git commit -m "Built at Travis-${TRAVIS_BUILD_NUMBER} (${TRAVIS_COMMIT_RANGE})"
+
+    # Push to gh-pages
+    git remote add origin-pages "https://${GH_TOKEN}@${GH_REPO}" > /dev/null 2>&1
+    git push --set-upstream origin-pages gh-pages
 fi
