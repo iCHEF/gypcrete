b894b24568e7117888e2068673333ebdcbccc6ef
CJ <cijies@gmail.com>
2017-07-06 17:30:24 +0800

    [examples] Show `onEscape` usage of <Popup>

--- examples/Popup/index.js
+++ examples/Popup/index.js
@@ -5,11 +5,17 @@ import { storiesOf } from '@storybook/react';
 import Popup, { EscapablePopup, PurePopup } from 'src/Popup';
 
 import BasicPopupExample from './BasicPopup';
+import EscapablePopupExample from './EscapablePopup';
 
 storiesOf('Popup', module)
     .addWithInfo(
         'basic usage',
         () => <BasicPopupExample />
+    )
+    .addWithInfo(
+        'escapable popup',
+        'Key `Esc` to close.',
+        () => <EscapablePopupExample />
     )
     // Props table
     .addPropsTable(() => <Popup />, [EscapablePopup, PurePopup]);
