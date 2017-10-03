5b390dce955dab5b14395e0d4ace8186f5e8f295
Teng-hao Chang <zhusee2@gmail.com>
2017-10-03 16:01:03 +0800

    Add helper to get all state class names from specific props

---
+++ src/utils/__tests__/getStateClassnames.test.js
@@ -0,0 +1,17 @@
+import getStateClassnames from '../getStateClassnames';
+
+it('can generates a single class name', () => {
+    expect(getStateClassnames({ active: true })).toBe('gyp-state-active');
+    expect(getStateClassnames({ highlight: true })).toBe('gyp-state-highlight');
+    expect(getStateClassnames({ error: true })).toBe('gyp-state-error');
+    expect(getStateClassnames({ disabled: true })).toBe('gyp-state-disabled');
+    expect(getStateClassnames({ untouchable: true })).toBe('gyp-state-untouchable');
+});
+
+it('can generate a set of class names', () => {
+    expect(getStateClassnames({ active: true, highlight: true }))
+        .toBe('gyp-state-active gyp-state-highlight');
+
+    expect(getStateClassnames({ error: true, disabled: true, untouchable: true }))
+        .toBe('gyp-state-error gyp-state-disabled gyp-state-untouchable');
+});
