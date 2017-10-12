287694253d3a674b33986d051c01423000b8e19e
Teng-hao Chang <zhusee2@gmail.com>
2017-10-12 11:49:27 +0800

    Update examples for form <TextInput>

--- packages/storybook/examples/Form.TextInput/BasicUsage.js
+++ packages/storybook/examples/Form.TextInput/BasicUsage.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
+import List from '@ichef/gypcrete/src/List';
 import TextInput from '@ichef/gypcrete-form/src/TextInput';
 
 function BasicUsage() {
     return (
-        <TextInput
-            label="Module name"
-            defaultValue="Points module" />
+        <List title="Configs">
+            <TextInput
+                label="Module name"
+                defaultValue="Points module" />
+
+            <TextInput
+                disabled
+                label="Disabled row"
+                value="Points module" />
+
+            <TextInput
+                readOnly
+                label="Read-only row"
+                value="Points module" />
+
+
+            <TextInput
+                label="Secret code"
+                value="Foo bar"
+                status="error"
+                errorMsg="Cannot authenticate with this code." />
+        </List>
     );
 }
 
