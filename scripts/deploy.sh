81066ff2ae4988ee0ec0aa8ad62b26cc85a07616
Teng-hao Chang <zhusee2@gmail.com>
2017-10-20 17:42:48 +0800

    Checkout to master before running lerna publish

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,6 @@ git config --global user.name "ichefbot"
 git config --global user.email "developer@ichef.com.tw"
 git remote add origin-pages "https://${GH_TOKEN}@${GH_REPO}" > /dev/null 2>&1
 
+git checkout master
 yarn release --git-remote origin-pages
 yarn ghpages
