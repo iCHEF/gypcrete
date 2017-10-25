310004154a9ae7cf622453bf99d1859d0a1aab26
Teng-hao Chang <zhusee2@gmail.com>
2017-10-25 16:22:05 +0800

    Export <SelectList> and <SelectOption>

--- packages/form/src/index.js
+++ packages/form/src/index.js
@@ -1,7 +1,13 @@
 import TextInputRow from './TextInputRow';
 import SwitchRow from './SwitchRow';
 
+import SelectList from './SelectList';
+import SelectOption from './SelectOption';
+
 export {
     TextInputRow,
     SwitchRow,
+
+    SelectList,
+    SelectOption,
 };
