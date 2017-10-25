30b920895fdaeadc4e9b13257f8424ad71783643
Teng-hao Chang <zhusee2@gmail.com>
2017-10-25 13:33:18 +0800

    Add examples for uncontrolled multiple <SelectList>

--- packages/storybook/examples/Form.SelectList/index.js
+++ packages/storybook/examples/Form.SelectList/index.js
@@ -7,9 +7,11 @@ import SelectList from '@ichef/gypcrete-form/src/SelectList';
 
 import SingleControlled from './SingleControlled';
 import SingleUncontrolled from './SingleUncontrolled';
+import MultipleUncontrolled from './MultipleUncontrolled';
 
 storiesOf('[Form] SelectList', module)
     .add('single (uncontrolled)', withInfo()(SingleUncontrolled))
     .add('single (controlled)', withInfo()(SingleControlled))
+    .add('multiple (uncontrolled)', withInfo()(MultipleUncontrolled))
     // Props table
     .addPropsTable(() => <SelectList />);
