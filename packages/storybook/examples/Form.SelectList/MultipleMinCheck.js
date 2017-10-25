70977dc330c5e5ffd6033eb718ffcb32454cd0a1
Teng-hao Chang <zhusee2@gmail.com>
2017-10-25 14:07:00 +0800

    Update examples for <SelectList>

---
+++ packages/storybook/examples/Form.SelectList/MultipleMinCheck.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import SelectList, { Option } from '@ichef/gypcrete-form/src/SelectList';
+
+function MultipleMinCheck() {
+    return (
+        <SelectList
+            multiple
+            minCheck={1}
+            defaultValues={['1']}
+            onChange={action('change')}>
+            <Option label="Option A" value="1" />
+            <Option label="Option B" value="2" />
+            <Option label="Option C" value="3" />
+        </SelectList>
+    );
+}
+
+export default MultipleMinCheck;
--- packages/storybook/examples/Form.SelectList/index.js
+++ packages/storybook/examples/Form.SelectList/index.js
@@ -8,10 +8,16 @@ import SelectList from '@ichef/gypcrete-form/src/SelectList';
 import SingleControlled from './SingleControlled';
 import SingleUncontrolled from './SingleUncontrolled';
 import MultipleUncontrolled from './MultipleUncontrolled';
+import MultipleControlled from './MultipleControlled';
+import MultipleReadOnlyOption from './MultipleReadOnlyOption';
+import MultipleMinCheck from './MultipleMinCheck';
 
 storiesOf('[Form] SelectList', module)
     .add('single (uncontrolled)', withInfo()(SingleUncontrolled))
     .add('single (controlled)', withInfo()(SingleControlled))
     .add('multiple (uncontrolled)', withInfo()(MultipleUncontrolled))
+    .add('multiple (controlled)', withInfo()(MultipleControlled))
+    .add('with read-only options', withInfo()(MultipleReadOnlyOption))
+    .add('has minimum checks', withInfo()(MultipleMinCheck))
     // Props table
     .addPropsTable(() => <SelectList />);
