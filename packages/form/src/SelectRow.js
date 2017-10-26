61e688f625146f87a6b16bf52f9531018098f517
Teng-hao Chang <zhusee2@gmail.com>
2017-10-26 18:11:43 +0800

    [form] Override popover position for <SelectRow>

--- packages/form/src/SelectRow.js
+++ packages/form/src/SelectRow.js
@@ -14,11 +14,20 @@ import { PurePopover } from '@ichef/gypcrete/lib/Popover';
 import anchored from '@ichef/gypcrete/lib/mixins/anchored';
 import closable from '@ichef/gypcrete/lib/mixins/closable';
 import renderToLayer from '@ichef/gypcrete/lib/mixins/renderToLayer';
+import prefixClass from '@ichef/gypcrete/lib/utils/prefixClass';
+import icBEM from '@ichef/gypcrete/lib/utils/icBEM';
 
 import SelectList from './SelectList';
 
 import formRow, { rowPropTypes } from './mixins/formRow';
 import './styles/SelectRow.scss';
+
+export const COMPONENT_NAME = prefixClass('form-select');
+const ROOT_BEM = icBEM(COMPONENT_NAME);
+export const BEM = {
+    root: ROOT_BEM,
+    popover: ROOT_BEM.element('popover'),
+};
 
 export const Popover = renderToLayer(
     closable({
@@ -61,6 +70,7 @@ class SelectRow extends PureComponent {
         return (
             <Popover
                 anchor={this.anchorNode}
+                className={BEM.popover.toString()}
                 onClose={this.handlePopoverClose}>
                 <SelectList
                     onChange={this.handleSelectChange}
@@ -83,7 +93,7 @@ class SelectRow extends PureComponent {
         const { popoverOpen } = this.state;
 
         const wrapperClassName = classNames(
-            'gyp-form-select',
+            COMPONENT_NAME,
             className,
         );
 
