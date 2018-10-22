3c38afd95a11d763f1a2fdc937055581c54bf6e6
Teng-hao Chang <zhusee2@gmail.com>
2018-10-22 16:32:19 +0800

    Add examples of <SplitView> working with <ColumnView>

---
+++ packages/storybook/examples/core/SplitView/InsideColumnView.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import SplitView from '@ichef/gypcrete/src/SplitView';
+import SplitViewColumn from '@ichef/gypcrete/src/SplitViewColumn';
+
+import DebugBox from 'utils/DebugBox';
+import ColoredBox from 'utils/ColoredBox';
+
+import DemoColumnView from './DemoColumnView';
+
+function InsideColumnView() {
+    return (
+        <DebugBox width="40rem" height="24rem">
+            <DemoColumnView>
+                <SplitView>
+                    <SplitViewColumn>
+                        <ColoredBox
+                            width="100%"
+                            height="30rem"
+                            color="rgb(255, 235, 235)">
+                            Narrow Column
+                        </ColoredBox>
+                    </SplitViewColumn>
+
+                    <SplitViewColumn wide>
+                        <ColoredBox
+                            width="100%"
+                            height="30rem"
+                            color="rgb(235, 245, 255)">
+                            Narrow Column
+                        </ColoredBox>
+                    </SplitViewColumn>
+                </SplitView>
+            </DemoColumnView>
+        </DebugBox>
+    );
+}
+
+export default InsideColumnView;
--- packages/storybook/examples/core/SplitView/index.js
+++ packages/storybook/examples/core/SplitView/index.js
@@ -6,7 +6,11 @@ import SplitViewColumn from '@ichef/gypcrete/src/SplitViewColumn';
 import getPropTables from 'utils/getPropTables';
 
 import BasicUsage from './BasicUsage';
+import ContainsColumnView from './ContainsColumnView';
+import InsideColumnView from './InsideColumnView';
 
 storiesOf('@ichef/gypcrete|SplitView', module)
     .add('basic usage', withInfo()(BasicUsage))
+    .add('contains <ColumnView>', withInfo()(ContainsColumnView))
+    .add('inside <ColumnView>', withInfo()(InsideColumnView))
     .add('props', getPropTables([SplitView, SplitViewColumn]));
