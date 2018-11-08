d1e82d2a4a90a55016357602a5442c1b6a6e1ad2
Teng-hao Chang <zhusee2@gmail.com>
2018-11-08 15:44:16 +0800

    Prevent error msg about setting ref on function components during tests

--- packages/form/src/mixins/formRow.js
+++ packages/form/src/mixins/formRow.js
@@ -36,9 +36,7 @@ const formRow = ({
         }
 
         handleRef = (ref) => {
-            if (withRef) {
-                this.componentRef = ref;
-            }
+            this.componentRef = ref;
         }
 
         render() {
@@ -62,7 +60,7 @@ const formRow = ({
 
             return (
                 <WrappedComponent
-                    ref={this.handleRef}
+                    ref={withRef ? this.handleRef : undefined}
                     ineditable={ineditable}
                     disabled={disabled}
                     readOnly={readOnly}
