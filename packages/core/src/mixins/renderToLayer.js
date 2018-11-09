1b3901b32dbf54091a72949f696fa9ab7c54c6cc
Teng-hao Chang <zhusee2@gmail.com>
2018-11-09 16:21:08 +0800

    Rewrite 'renderToLayer()' mixin to use React.createPortal() instead

--- packages/core/src/mixins/renderToLayer.js
+++ packages/core/src/mixins/renderToLayer.js
@@ -1,28 +1,5 @@
-/**
- * renderToLayer() HOC mixin
- * =========================
- * Render a component to an node outside of React root, while still being
- * maintained under React virtual tree.
- *
- * Inspired by material-ui
- * @ref https://github.com/callemall/material-ui/blob/master/src/internal/RenderToLayer.js
- *
- * Original created by @cjies
- * @ref https://github.com/iCHEF/iC-framework-react/pull/66
- *
- * Converted to mixin pattern by @zhusee2
- * @ref https://facebook.github.io/react/blog/2016/07/13/mixins-considered-harmful.html
- *
- * Usage
- * -----
- * const ExternalComponent = renderToLayer(Component);
- */
-
 import React, { Component } from 'react';
-
-// Importing an unstable API method from react-dom
-// eslint-disable-next-line camelcase
-import ReactDOM, { unstable_renderSubtreeIntoContainer } from 'react-dom';
+import { createPortal } from 'react-dom';
 
 import prefixClass from '../utils/prefixClass';
 import getComponentName from '../utils/getComponentName';
@@ -33,79 +10,49 @@ import '../styles/RenderToLayer.scss';
 const COMPONENT_NAME = prefixClass('base-layer');
 const LAYER_ID_PREFIX = 'layer';
 
+export function createLayer() {
+    const layer = document.createElement('div');
+    layer.className = COMPONENT_NAME;
+    layer.id = randId({ prefix: LAYER_ID_PREFIX });
+
+    return layer;
+}
+
+/**
+ * renderToLayer() HOC mixin
+ * =========================
+ * Render a component to an node outside of React root,
+ * using `React.createPortal()`.
+ *
+ * @param {React.ComponentType<any>} WrappedComponent
+ *
+ * @example
+ * const ExternalComponent = renderToLayer(Component);
+ */
 function renderToLayer(WrappedComponent) {
     const componentName = getComponentName(WrappedComponent);
 
     class RenderToLayer extends Component {
         static displayName = `renderToLayer(${componentName})`;
 
-        constructor(...args) {
-            super(...args);
-
-            this.baseLayer = null;
-            this.componentRef = null;
+        constructor(props) {
+            super(props);
+            this.baseLayer = createLayer();
         }
 
         componentDidMount() {
-            this.createBaseLayer();
-            this.renderComponentToLayer();
-        }
-
-        componentWillReceiveProps(nextProps) {
-            this.renderComponentToLayer({ withProps: nextProps });
+            document.body.appendChild(this.baseLayer);
         }
 
         componentWillUnmount() {
-            if (!this.baseLayer) return;
-
-            ReactDOM.unmountComponentAtNode(this.baseLayer);
-            this.removeBaseLayer();
-        }
-
-        // -------------------------------------
-        //   Base Layer
-        // -------------------------------------
-
-        /**
-         * Create the base layer on <body> to render <Componenet>
-         */
-        createBaseLayer() {
-            const baseLayer = document.createElement('div');
-            baseLayer.className = COMPONENT_NAME;
-            baseLayer.id = randId({ prefix: LAYER_ID_PREFIX });
-
-            this.baseLayer = baseLayer;
-            document.body.appendChild(baseLayer);
-        }
-
-        /**
-         * Remove base layer from <body>
-         */
-        removeBaseLayer() {
             document.body.removeChild(this.baseLayer);
-            this.baseLayer = null;
-        }
-
-        // -------------------------------------
-        //   Render
-        // -------------------------------------
-
-        /**
-         * Renders passed-in <Componenet> to the external base layer,
-         * based on given props.
-         *
-         * @param {Object} withProps - current or next props of <RenderToLayer> wrapper
-         */
-        renderComponentToLayer({ withProps = this.props } = {}) {
-            this.componentRef = unstable_renderSubtreeIntoContainer(
-                this, // parentComponent
-                <WrappedComponent {...withProps} />,
-                this.baseLayer // container
-            );
         }
 
         render() {
-            return null;
+            return createPortal(
+                <WrappedComponent {...this.props} />,
+                this.baseLayer,
+            );
         }
     }
 
