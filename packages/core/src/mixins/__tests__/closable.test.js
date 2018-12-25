38380da6da44630cdea16d0844b1be2291fb481b
Teng-hao Chang <zhusee2@gmail.com>
2018-12-25 19:35:02 +0800

    Refactor tests for 'closable()' mixin to cover all config combinations

--- packages/core/src/mixins/__tests__/closable.test.js
+++ packages/core/src/mixins/__tests__/closable.test.js
@@ -48,123 +48,6 @@ it('takes runtime options', () => {
     });
 });
 
-it('triggers onClose() call on Escape key up if turned on', () => {
-    const ClosableFoo = closable({ onEscape: true })(Foo);
-    const handleClose = jest.fn();
-
-    mount(<ClosableFoo onClose={handleClose} />);
-    expect(handleClose).not.toHaveBeenCalled();
-
-    // Dispatch 'Enter' keyboard event, nothing happened
-    let keyEvent = new KeyboardEvent('keyup', { keyCode: keycode('Enter') });
-    document.dispatchEvent(keyEvent);
-    expect(handleClose).toHaveBeenCalledTimes(0);
-
-    // Dispatch 'Escape' keyboard event, triggers onClose()
-    keyEvent = new KeyboardEvent('keyup', { keyCode: keycode('Escape') });
-    document.dispatchEvent(keyEvent);
-    expect(handleClose).toHaveBeenCalledTimes(1);
-});
-
-it('does not trigger onClose() call on Escape key up if turned off', () => {
-    const ClosableFoo = closable({ onEscape: false })(Foo);
-    const handleClose = jest.fn();
-
-    mount(<ClosableFoo onClose={handleClose} />);
-    expect(handleClose).not.toHaveBeenCalled();
-
-    let keyEvent = new KeyboardEvent('keyup', { keyCode: keycode('Enter') });
-    document.dispatchEvent(keyEvent);
-    expect(handleClose).toHaveBeenCalledTimes(0);
-
-    keyEvent = new KeyboardEvent('keyup', { keyCode: keycode('Escape') });
-    document.dispatchEvent(keyEvent);
-    expect(handleClose).toHaveBeenCalledTimes(0);
-});
-
-it('triggers onClose() call on click/touch outside after a delay if turned on', () => {
-    const ClosableFoo = closable({ onClickOutside: true })(Foo);
-    const handleClose = jest.fn();
-
-    mount(<ClosableFoo onClose={handleClose} />);
-    expect(handleClose).not.toHaveBeenCalled();
-
-    let event = new MouseEvent('click');
-    document.dispatchEvent(event);
-
-    jest.runOnlyPendingTimers();
-    expect(handleClose).toHaveBeenCalledTimes(1);
-
-    // jsdom doesn't support constructing TouchEvent yet.
-    event = new CustomEvent('touchend');
-    document.dispatchEvent(event);
-
-    jest.runOnlyPendingTimers();
-    expect(handleClose).toHaveBeenCalledTimes(2);
-});
-
-it('does not trigger onClose() call on any click/touch outside after a delay if turned off', () => {
-    const ClosableFoo = closable({ onClickOutside: false })(Foo);
-    const handleClose = jest.fn();
-
-    mount(<ClosableFoo onClose={handleClose} />);
-    expect(handleClose).not.toHaveBeenCalled();
-
-    let event = new MouseEvent('click');
-    document.dispatchEvent(event);
-
-    jest.runOnlyPendingTimers();
-    expect(handleClose).toHaveBeenCalledTimes(0);
-
-    event = new CustomEvent('touchstart');
-    document.dispatchEvent(event);
-
-    jest.runOnlyPendingTimers();
-    expect(handleClose).toHaveBeenCalledTimes(0);
-});
-
-it('triggers onClose() call on click/touch inside a delay if turned on', () => {
-    const ClosableFoo = closable({ onClickInside: true })(Foo);
-    const handleClose = jest.fn();
-
-    const wrapper = mount(<ClosableFoo onClose={handleClose} />);
-    expect(handleClose).not.toHaveBeenCalled();
-
-    let event = new MouseEvent('click');
-    wrapper.instance().nodeRef.dispatchEvent(event);
-
-    jest.runOnlyPendingTimers();
-    expect(handleClose).toHaveBeenCalledTimes(1);
-
-    // jsdom doesn't support constructing TouchEvent yet.
-    event = new CustomEvent('touchend');
-    wrapper.instance().nodeRef.dispatchEvent(event);
-
-    jest.runOnlyPendingTimers();
-    expect(handleClose).toHaveBeenCalledTimes(2);
-});
-
-it('triggers onClose() call on click/touch inside a delay if turned off', () => {
-    const ClosableFoo = closable({ onClickInside: false })(Foo);
-    const handleClose = jest.fn();
-
-    const wrapper = mount(<ClosableFoo onClose={handleClose} />);
-    expect(handleClose).not.toHaveBeenCalled();
-
-    let event = new MouseEvent('click');
-    wrapper.instance().nodeRef.dispatchEvent(event);
-
-    jest.runOnlyPendingTimers();
-    expect(handleClose).toHaveBeenCalledTimes(0);
-
-    // jsdom doesn't support constructing TouchEvent yet.
-    event = new CustomEvent('touchstart');
-    wrapper.instance().nodeRef.dispatchEvent(event);
-
-    jest.runOnlyPendingTimers();
-    expect(handleClose).toHaveBeenCalledTimes(0);
-});
-
 it('tears down event listeners on unmount', () => {
     const ClosableFoo = closable({
         onEscape: true,
@@ -188,4 +71,143 @@ it('tears down event listeners on unmount', () => {
 
     jest.runAllTimers();
     expect(handleClose).not.toHaveBeenCalled();
+});
+
+describe.each`
+    onEscape | shouldBeCalled | desc
+    ${true}  | ${true}        | ${'should'}
+    ${false} | ${false}       | ${'should not'}
+`('$desc call onClose() for ESC keys when onEscape=$onEscape', ({ onEscape, shouldBeCalled }) => {
+    const ClosableFoo = closable({ onEscape })(Foo);
+    const rootNode = document.createElement('div');
+
+    beforeAll(() => {
+        document.body.appendChild(rootNode);
+    });
+
+    afterAll(() => {
+        document.body.removeChild(rootNode);
+    });
+
+    it.each([
+        { onClickInside: true, onClickOutside: true },
+        { onClickInside: true, onClickOutside: false },
+        { onClickInside: false, onClickOutside: true },
+        { onClickInside: false, onClickOutside: true },
+    ])('when %p', ({ onClickInside, onClickOutside }) => {
+        const handleClose = jest.fn();
+        const closableOptions = { onClickInside, onClickOutside };
+
+        const wrapper = mount(
+            <ClosableFoo closable={closableOptions} onClose={handleClose} />,
+            { attachTo: rootNode },
+        );
+
+        // Dispatch 'Enter' keyboard event, nothing happened
+        let keyEvent = new KeyboardEvent('keyup', { keyCode: keycode('Enter') });
+        document.dispatchEvent(keyEvent);
+        expect(handleClose).toHaveBeenCalledTimes(0);
+
+        // Dispatch 'Escape' keyboard event, triggers onClose()
+        keyEvent = new KeyboardEvent('keyup', { keyCode: keycode('Escape') });
+        document.dispatchEvent(keyEvent);
+        expect(handleClose).toHaveBeenCalledTimes(shouldBeCalled ? 1 : 0);
+
+        wrapper.unmount();
+    });
+});
+
+
+describe.each`
+    onClickInside | shouldBeCalled | desc
+    ${true}       | ${true}        | ${'should'}
+    ${false}      | ${false}       | ${'should not'}
+`('$desc call onClose() for inside-clicks when onClickInside=$onClickInside', ({ onClickInside, shouldBeCalled }) => {
+    const ClosableFoo = closable({ onClickInside })(Foo);
+    const rootNode = document.createElement('div');
+
+    beforeAll(() => {
+        document.body.appendChild(rootNode);
+    });
+
+    afterAll(() => {
+        document.body.removeChild(rootNode);
+    });
+
+    it.each([
+        { onEscape: true, onClickOutside: true },
+        { onEscape: true, onClickOutside: false },
+        { onEscape: false, onClickOutside: true },
+        { onEscape: false, onClickOutside: true },
+    ])('when %p', ({ onEscape, onClickOutside }) => {
+        const handleClose = jest.fn();
+        const closableOptions = { onEscape, onClickOutside };
+
+        const wrapper = mount(
+            <ClosableFoo closable={closableOptions} onClose={handleClose} />,
+            { attachTo: rootNode },
+        );
+
+        let event = new MouseEvent('click', { bubbles: true });
+        wrapper.instance().nodeRef.dispatchEvent(event);
+
+        jest.runOnlyPendingTimers();
+        expect(handleClose).toHaveBeenCalledTimes(shouldBeCalled ? 1 : 0);
+
+        // jsdom doesn't support constructing TouchEvent yet.
+        event = new CustomEvent('touchend', { bubbles: true });
+        wrapper.instance().nodeRef.dispatchEvent(event);
+
+        jest.runOnlyPendingTimers();
+        expect(handleClose).toHaveBeenCalledTimes(shouldBeCalled ? 2 : 0);
+
+        wrapper.unmount();
+    });
+});
+
+describe.each`
+    onClickOutside | shouldBeCalled | desc
+    ${true}        | ${true}        | ${'should'}
+    ${false}       | ${false}       | ${'should not'}
+`('$desc call onClose() when onClickOutside=$onClickOutside', ({ onClickOutside, shouldBeCalled }) => {
+    const ClosableFoo = closable({ onClickOutside })(Foo);
+    const rootNode = document.createElement('div');
+
+    beforeAll(() => {
+        document.body.appendChild(rootNode);
+    });
+
+    afterAll(() => {
+        document.body.removeChild(rootNode);
+    });
+
+    it.each([
+        { onEscape: true, onClickInside: true },
+        { onEscape: true, onClickInside: false },
+        { onEscape: false, onClickInside: true },
+        { onEscape: false, onClickInside: true },
+    ])('when %p', ({ onEscape, onClickInside }) => {
+        const handleClose = jest.fn();
+        const closableOptions = { onEscape, onClickInside };
+
+        const wrapper = mount(
+            <ClosableFoo closable={closableOptions} onClose={handleClose} />,
+            { attachTo: rootNode },
+        );
+
+        let event = new MouseEvent('click');
+        document.dispatchEvent(event);
+
+        jest.runOnlyPendingTimers();
+        expect(handleClose).toHaveBeenCalledTimes(shouldBeCalled ? 1 : 0);
+
+        // jsdom doesn't support constructing TouchEvent yet.
+        event = new CustomEvent('touchend');
+        document.dispatchEvent(event);
+
+        jest.runOnlyPendingTimers();
+        expect(handleClose).toHaveBeenCalledTimes(shouldBeCalled ? 2 : 0);
+
+        wrapper.unmount();
+    });
 });
