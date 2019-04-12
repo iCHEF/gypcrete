be33cf70339af4c43e96f50915b17a2548ec2590
Teng-hao Chang <zhusee2@gmail.com>
2019-04-12 17:31:46 +0800

    Update form <TextInputRow> to render a core <TextInput> instead

--- packages/form/src/TextInputRow.js
+++ packages/form/src/TextInputRow.js
@@ -1,154 +1,36 @@
 /* eslint-disable class-methods-use-this */
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
-import { ListRow, TextLabel } from '@ichef/gypcrete';
-import AutoSizeTextarea from 'react-textarea-autosize';
-
-import prefixClass from '@ichef/gypcrete/lib/utils/prefixClass';
-import icBEM from '@ichef/gypcrete/lib/utils/icBEM';
+import { ListRow, TextInput } from '@ichef/gypcrete';
 
 import formRow, { rowPropTypes } from './mixins/formRow';
-import './styles/TextInputRow.scss';
 
-export const COMPONENT_NAME = prefixClass('form-text-input');
-const ROOT_BEM = icBEM(COMPONENT_NAME);
-export const BEM = {
-    root: ROOT_BEM,
-    label: ROOT_BEM.element('label'),
-    input: ROOT_BEM.element('input'),
-};
-
-const isFunction = func => (typeof func === 'function');
-
-class TextInputRow extends PureComponent {
-    static propTypes = {
-        label: PropTypes.node.isRequired,
-        multiLine: PropTypes.bool,
-        minRows: PropTypes.number,
-        maxRows: PropTypes.number,
-        renderInput: PropTypes.func,
-        inputComponent: PropTypes.oneOfType([
-            PropTypes.func,
-            PropTypes.string,
-        ]),
-        // input props
-        value: PropTypes.string,
-        placeholder: PropTypes.string,
-        onChange: PropTypes.func,
-        onFocus: PropTypes.func,
-        onBlur: PropTypes.func,
-        // from formRow()
-        ineditable: PropTypes.bool,
-        rowProps: rowPropTypes,
-    };
-
-    static defaultProps = {
-        multiLine: false,
-        minRows: 2,
-        maxRows: undefined,
-        renderInput: undefined,
-        inputComponent: 'input',
-        value: undefined,
-        placeholder: 'Unset',
-        onChange: () => {},
-        onFocus: () => {},
-        onBlur: () => {},
-        ineditable: false,
-        rowProps: {},
-    };
-
-    state = {
-        focused: false,
-    };
-
-    handleInputFocus = (event) => {
-        this.setState({ focused: true });
-        this.props.onFocus(event);
-    }
-
-    handleInputBlur = (event) => {
-        this.setState({ focused: false });
-        this.props.onBlur(event);
-    }
-
-    renderInput({
-        multiLine,
-        style = {},
-        renderInput,
-        inputComponent,
-        minRows,
-        maxRows,
-        ...otherProps
-    }) {
-        const sharedProps = {
-            className: BEM.input.toString(),
-            style,
-            onFocus: this.handleInputFocus,
-            onBlur: this.handleInputBlur,
-            ...otherProps
-        };
-
-        if (multiLine) {
-            const textareaProps = {
-                ...sharedProps,
-                minRows,
-                maxRows,
-            };
-
-            return <AutoSizeTextarea {...textareaProps} />;
-        }
-
-        const inputProps = {
-            type: 'text',
-            ...sharedProps,
-        };
-
-        if (isFunction(renderInput)) {
-            return renderInput(inputProps);
-        }
-
-        return React.createElement(inputComponent, inputProps);
-    }
-
-    render() {
-        const {
-            label,
-            onFocus,
-            onBlur,
-            // row props
-            ineditable,
-            rowProps,
-            // React props
-            className,
-            children,
-            ...renderInputProps
-        } = this.props;
-
-        const bemClass = BEM.root
-            .modifier('focused', this.state.focused)
-            .modifier('ineditable', ineditable);
-        const rootClassName = classNames(bemClass.toString(), className);
-
-        const keyLabel = (
-            <span className={BEM.label.toString()}>
-                {label}
-            </span>
-        );
-        const input = this.renderInput(renderInputProps);
-
-        return (
-            <ListRow className={rootClassName} {...rowProps}>
-                <TextLabel
-                    basic={keyLabel}
-                    aside={input}
-                />
-                {children}
-            </ListRow>
-        );
-    }
+function TextInputRow({
+    // from formRow()
+    ineditable, // unwanted prop
+    readOnly,
+    disabled,
+    rowProps,
+    ...inputProps
+}) {
+    return (
+        <ListRow {...rowProps}>
+            <TextInput
+                readOnly={readOnly}
+                disabled={disabled}
+                {...inputProps}
+            />
+        </ListRow>
+    );
 }
 
+TextInputRow.propTypes = {
+    // from formRow()
+    readOnly: PropTypes.bool.isRequired,
+    disabled: PropTypes.bool.isRequired,
+    rowProps: rowPropTypes.isRequired,
+};
+
 export { TextInputRow as PureTextInputRow };
-export default formRow({ withRef: true })(TextInputRow);
+export default formRow()(TextInputRow);
--- packages/storybook/examples/form/TextInputRow/BasicUsage.js
+++ packages/storybook/examples/form/TextInputRow/BasicUsage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
 
 import List from '@ichef/gypcrete/src/List';
 import TextInputRow from '@ichef/gypcrete-form/src/TextInputRow';
@@ -9,26 +10,34 @@ function BasicUsage() {
         <List title="Configs">
             <TextInputRow
                 label="Module name"
-                defaultValue="Points module" />
+                defaultValue="Points module"
+                onChange={action('change')}
+            />
 
             <ControlledRow initValue="Points module" />
 
             <TextInputRow
                 disabled
                 label="Disabled row"
-                value="Points module" />
+                value="Points module"
+                onChange={action('change')}
+            />
 
             <TextInputRow
                 readOnly
                 label="Read-only row"
-                value="Points module" />
+                value="Points module"
+                onChange={action('change')}
+            />
 
 
             <TextInputRow
                 label="Secret code"
                 value="Foo bar"
+                onChange={action('change')}
                 status="error"
-                errorMsg="Cannot authenticate with this code." />
+                errorMsg="Cannot authenticate with this code."
+            />
         </List>
     );
 }
