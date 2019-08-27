7192fc434b0f3d63428b446863a2b8b061e00ae0
Teng-hao Chang <zhusee2@gmail.com>
2019-08-27 18:36:15 +0800

    Remove incorrect type annotation of 'wrapIfNotElement()'

--- packages/core/src/utils/wrapIfNotElement.js
+++ packages/core/src/utils/wrapIfNotElement.js
@@ -3,12 +3,6 @@ import React from 'react';
 /**
  * Wrap the passed-in `content` with a component if it's not a React element,
  * to make sure the result will always be a HTML tag.
- *
- * @param {ReactChildren} content
- * @param {Component} Wrapper
- * @param {String} prop - pass `content` into specified `prop`. Default via 'children'.
- *
- * @return {Element}
  */
 function wrapIfNotElement(content, {
     with: Wrapper,
