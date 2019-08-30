8e852d89106ecb346c5630b53a94e2d940a7ecc9
Teng-hao Chang <zhusee2@gmail.com>
2019-08-30 18:18:35 +0800

    Fix props table in <SplitView> example wrapped by unwanted decorators

--- packages/storybook/examples/core/SplitView/index.js
+++ packages/storybook/examples/core/SplitView/index.js
@@ -6,21 +6,33 @@ import SplitView from '@ichef/gypcrete/src/SplitView';
 import SplitViewColumn from '@ichef/gypcrete/src/SplitViewColumn';
 
 import DebugBox from 'utils/DebugBox';
-import getPropTables from 'utils/getPropTables';
+import { getAddonOptions } from 'utils/getPropTables';
 
 import BasicUsage from './BasicUsage';
 import ContainsColumnView from './ContainsColumnView';
 import InsideColumnView from './InsideColumnView';
 
 storiesOf('@ichef/gypcrete|SplitView', module)
-    .addDecorator(storyFn => (
-        <DebugBox width="40rem" height="24rem">
-            {storyFn()}
-        </DebugBox>
-    ))
+    .addDecorator((storyFn, { parameters = {} }) => {
+        if (parameters.debugBox === false) {
+            return storyFn();
+        }
+        return (
+            <DebugBox width="40rem" height="24rem">
+                {storyFn()}
+            </DebugBox>
+        );
+    })
     .addDecorator(withInfo)
 
     .add('basic usage', BasicUsage)
     .add('contains <ColumnView>', ContainsColumnView)
     .add('inside <ColumnView>', InsideColumnView)
-    .add('props', getPropTables([SplitView, SplitViewColumn]));
+    .add(
+        'props',
+        () => <div />,
+        {
+            info: getAddonOptions([SplitView, SplitViewColumn]),
+            debugBox: false,
+        }
+    );
