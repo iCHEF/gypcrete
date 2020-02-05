352f35fee96655e71989e1152154c4b2fa04abc6
chenesan <pipio1994@gmail.com>
2020-02-05 16:40:54 +0800

    Simplify custom tag button storybook example

--- packages/storybook/examples/core/Button.stories.js
+++ packages/storybook/examples/core/Button.stories.js
@@ -73,72 +73,31 @@ export function ButtonWithStatus() {
     );
 }
 
-function NormalButton(props) {
-    return (
-        <Button
-            basic="Button"
-            aside="<button>"
-            tag="Tag"
-            tagName="button"
-            {...props} />
-    );
-}
-
-function DivButton(props) {
-    return (
-        <Button
-            basic="Button"
-            aside="<div>"
-            tag="Tag"
-            tagName="div"
-            {...props} />
-    );
-}
-
-function LinkButton(props) {
-    return (
-        <Button
-            basic="Button"
-            aside="<a>"
-            tag="Tag"
-            tagName="a"
-            // props for <a>
-            href="http://ichefpos.com/"
-            target="_blank"
-            {...props} />
-    );
-}
-
 export function CustomTagButton() {
     return (
-        <div>
-            <FlexRow>
-                <NormalButton bold />
-                <NormalButton bold color="blue" />
-                <NormalButton bold color="red" />
-                <NormalButton solid />
-                <NormalButton solid color="blue" />
-                <NormalButton solid color="red" />
-            </FlexRow>
-
-            <FlexRow>
-                <DivButton bold />
-                <DivButton bold color="blue" />
-                <DivButton bold color="red" />
-                <DivButton solid />
-                <DivButton solid color="blue" />
-                <DivButton solid color="red" />
-            </FlexRow>
-
-            <FlexRow>
-                <LinkButton bold />
-                <LinkButton bold color="blue" />
-                <LinkButton bold color="red" />
-                <LinkButton solid />
-                <LinkButton solid color="blue" />
-                <LinkButton solid color="red" />
-            </FlexRow>
-        </div>
+        <FlexRow>
+            <Button
+                basic="Button"
+                aside="<button>"
+                tag="Tag"
+                tagName="button"
+            />
+            <Button
+                basic="Button"
+                aside="<a>"
+                tag="Tag"
+                tagName="a"
+                // props for <a>
+                href="http://ichefpos.com/"
+                target="_blank"
+            />
+            <Button
+                basic="Button"
+                aside="<div>"
+                tag="Tag"
+                tagName="div"
+            />
+        </FlexRow>
     );
 }
 
