6d8062caf6a268258cc8fc62dc6df1e0f4a80391
chenesan <pipio1994@gmail.com>
2020-02-06 18:18:47 +0800

    Add new SearchInput storybook doc

---
+++ packages/storybook/examples/core/SearchInput.stories.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { action } from '@storybook/addon-actions';
+
+import SearchInput, { PureSearchInput } from '@ichef/gypcrete/src/SearchInput';
+import DebugBox from 'utils/DebugBox';
+
+export default {
+    title: '@ichef/gypcrete|SearchInput',
+    component: PureSearchInput,
+    subcomponents: {
+        'rowComp()': SearchInput,
+    },
+};
+
+
+
+export function BasicSearchInputExample() {
+    const [controlledInputValue, setValue] = useState('');
+
+    const handleControlledInputChange = (e) => {
+        action('onChange')(e.target.value);
+        setValue(e.target.value);
+    };
+
+    const handleControlledInputReset = () => {
+        action('onReset')();
+        setValue('');
+    };
+
+    return (
+        <div>
+            <DebugBox>
+                <SearchInput
+                    value={controlledInputValue}
+                    onChange={handleControlledInputChange}
+                    onSearch={action('onSearch')}
+                    onReset={handleControlledInputReset}
+                    // searchOnInputChange
+                    searchOnInputBlur
+                    blockDuplicateValueSearch
+                    blockEmptyValueSearch
+                />
+            </DebugBox>
+
+            <DebugBox>
+                <SearchInput defaultValue="Monkey King" />
+            </DebugBox>
+
+            <DebugBox>
+                <SearchInput defaultValue="Monkey King" status="loading" />
+            </DebugBox>
+        </div>
+    );
+}
