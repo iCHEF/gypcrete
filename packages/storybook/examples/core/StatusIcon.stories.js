507073d39a20f53b7620a32a2d83bad067047332
chenesan <pipio1994@gmail.com>
2020-02-07 10:19:45 +0800

    Add new StatusIcon storybook doc

--- packages/storybook/examples/core/StatusIcon.stories.js
+++ packages/storybook/examples/core/StatusIcon.stories.js
@@ -3,22 +3,33 @@ import React from 'react';
 import StatusIcon from '@ichef/gypcrete/src/StatusIcon';
 import DebugBox from 'utils/DebugBox';
 
-function CornerStatusIconExample() {
+export default {
+    title: '@ichef/gypcrete|StatusIcon',
+    component: StatusIcon,
+};
+
+export function BasicStatusIcon() {
+    return (
+        <div>
+            <StatusIcon status="loading" />
+            <StatusIcon status="success" autohide={false} />
+            <StatusIcon status="error" />
+        </div>
+    );
+}
+
+export function StatusIconInCorner() {
     return (
         <div>
             <DebugBox width={32} height={32}>
                 <StatusIcon status="loading" position="corner" />
             </DebugBox>
-
             <DebugBox width={32} height={32}>
                 <StatusIcon status="success" position="corner" autohide={false} />
             </DebugBox>
-
             <DebugBox width={32} height={32}>
                 <StatusIcon status="error" position="corner" />
             </DebugBox>
         </div>
     );
 }
-
-export default CornerStatusIconExample;
