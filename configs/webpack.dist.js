bfb976fc05a2fab7ec982421a05995aaaee1849c
陳乙山 <pipio1994@gmail.com>
2020-08-20 11:38:52 +0800

    Add jsx extension in webpack config

--- configs/webpack.dist.js
+++ configs/webpack.dist.js
@@ -19,7 +19,7 @@ module.exports = {
     },
 
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json']
+        extensions: ['.ts', '.tsx', '.js', 'jsx', '.json']
     },
 
     module: {
