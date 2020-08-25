35c1ac076e0376eff41991f7c65456da3892141d
陳乙山 <pipio1994@gmail.com>
2020-08-25 14:38:05 +0800

    Add types on wrapIfNotElement

--- packages/core/src/utils/wrapIfNotElement.tsx
+++ packages/core/src/utils/wrapIfNotElement.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
 /**
  * Wrap the passed-in `content` with a component if it's not a React element,
  * to make sure the result will always be a HTML tag.
  */
-function wrapIfNotElement(content, {
+function wrapIfNotElement(content: any, {
     with: Wrapper,
     via: prop = 'children',
+}: {
+    with: React.ElementType<any>,
+    via?: string,
 }) {
     if (React.isValidElement(content)) {
         return content;
