9c8a2f912dd5145ec2aa0f638621a5061d1d3a8b
陳乙山 <pipio1994@gmail.com>
2020-08-25 15:57:50 +0800

    Add type on enhancedPropTypes

--- packages/core/src/utils/enhancedPropTypes.ts
+++ packages/core/src/utils/enhancedPropTypes.ts
@@ -5,13 +5,8 @@
 const EnhancedPropTypes = {
     /**
      * Check prop should be empty
-     *
-     * @param  {Object} props
-     * @param  {String} propName
-     * @param  {String} componentName
-     * @return {NULL}
      */
-    isEmpty: (props, propName, componentName) => {
+    isEmpty: (props: object, propName: string, componentName: string) => {
         if (props[propName]) {
             return new Error(`<${componentName}> must not contains ${propName}.`);
         }
