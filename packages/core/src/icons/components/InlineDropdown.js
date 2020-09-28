06c748959a33606ce9fb691b402073ffeb8ce619
Benny <benny0642@gmail.com>
2020-09-28 15:25:17 +0800

    Add inline-dropdown

---
+++ packages/core/src/icons/components/InlineDropdown.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SvgInlineDropdown(props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 32 32" fill="none" {...props}>
+      <path d="M0 0l28 16L0 32V0z" fill="currentColor" />
+    </svg>
+  );
+}
