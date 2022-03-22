b2e51ca1bbe82801a1ee510ee9a746e97f810e23
Leanne Chen <lianchennnn@gmail.com>
2022-03-22 17:23:42 +0800

    feat: change `fill` value to currentColor

--- packages/core/src/icons/components/AddLocation.js
+++ packages/core/src/icons/components/AddLocation.js
@@ -6,7 +6,7 @@ export default function SvgAddLocation(props) {
       width="1em"
       height="1em"
       viewBox="0 0 24 24"
-      fill="none"
+      fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
