cdfb6d4f4b269748675788f40f7780d2710f6911
Benny <benny0642@gmail.com>
2022-05-03 11:15:20 +0800

    feat: add SvgRadioEmpty and SvgRadioSelected

---
+++ packages/core/src/icons/components/RadioSelected.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+export default function SvgRadioSelected(props) {
+  return (
+    <svg width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <rect
+        x={7}
+        y={7}
+        width={18}
+        height={18}
+        rx={9}
+        stroke="currentColor"
+        strokeWidth={2}
+        fill="none"
+      />
+      <rect
+        x={11.5}
+        y={11.5}
+        width={9}
+        height={9}
+        rx={4.5}
+        stroke="currentColor"
+      />
+    </svg>
+  );
+}
