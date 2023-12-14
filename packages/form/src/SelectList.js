907b7a0e4320cd3913461b4f09d13f473a6858a5
DMoon <dmoon.t@gmail.com>
2023-12-14 19:59:17 +0800

    refactor: Refactor SelectList component to use React hooks

--- packages/form/src/SelectList.js
+++ packages/form/src/SelectList.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import warning from 'warning';
 
@@ -7,7 +7,6 @@ import { Map as ImmutableMap } from 'immutable';
 import {
   List,
 } from '@ichef/gypcrete';
-import getRemainingProps from '@ichef/gypcrete/lib/utils/getRemainingProps';
 
 import Option, {
   valueType,
@@ -34,217 +33,202 @@ function getInitialCheckedState(selectedValue, multiple = true) {
  * It can be in either **single** or **multiple** response mode.
  */
 
-class SelectList extends PureComponent {
-    static propTypes = {
-      multiple: PropTypes.bool,
-      showCheckAll: PropTypes.bool,
-      minCheck: PropTypes.number,
-      checkAllLabel: PropTypes.node,
-      value: PropTypes.oneOfType([
-        valueType,
-        PropTypes.arrayOf(valueType),
-      ]),
-      defaultValue: PropTypes.oneOfType([
-        valueType,
-        PropTypes.arrayOf(valueType),
-      ]),
-      onChange: PropTypes.func,
-      title: PropTypes.string,
-      desc: PropTypes.node,
-    };
+const SelectList = React.memo(({
+  multiple,
+  showCheckAll,
+  minCheck,
+  checkAllLabel,
+  value,
+  defaultValue,
+  onChange,
+  title,
+  desc,
+  children,
+  ...wrapperProps
+}) => {
+  const getInitialValue = () => {
+    if (value !== undefined) {
+      return value;
+    }
 
-    static defaultProps = {
-      multiple: false,
-      showCheckAll: true,
-      minCheck: 0,
-      checkAllLabel: 'All',
-      value: undefined,
-      defaultValue: undefined,
-      onChange: () => {},
-      title: undefined,
-      desc: undefined,
-    };
+    if (multiple && defaultValue === undefined) {
+      return [];
+    }
 
-    state = {
-      checkedState: getInitialCheckedState(
-        this.getInitialValue(),
-        this.props.multiple
-      ),
-    };
+    return defaultValue;
+  };
 
-    getInitialValue() {
-      const { value, defaultValue, multiple } = this.props;
+  const [checkedState, setCheckedState] = useState(getInitialCheckedState(
+    getInitialValue(),
+    multiple
+  ));
+  const prevMultiple = useRef(multiple);
+  const isControlled = value !== undefined;
+  const prevIsControlled = useRef(isControlled);
 
-      if (value !== undefined) {
-        return value;
+  const options = parseSelectOptions(children);
+
+  const isAllChecked = options.every(option => checkedState.get(option.value));
+
+  // Update state when value prop changes
+  useEffect(
+    () => {
+      if (isControlled) {
+        setCheckedState(getInitialCheckedState(value, multiple));
       }
+    },
+    [value, multiple, isControlled]
+  );
 
-      if (multiple && defaultValue === undefined) {
-        return [];
-      }
-
-      return defaultValue;
-    }
-
-    getIsControlled(fromProps = this.props) {
-      return fromProps.value !== undefined;
-    }
-
-    getIsAllChecked() {
-      const { checkedState } = this.state;
-      const allOptions = this.getOptions();
-
-      return allOptions.every(option => checkedState.get(option.value));
-    }
-
-    getOptions(fromProps = this.props) {
-      return parseSelectOptions(fromProps.children);
-    }
-
-    // eslint-disable-next-line react/destructuring-assignment
-    getValues(fromCheckedState = this.state.checkedState) {
-      const allOptions = this.getOptions();
-
-      return allOptions
-        .filter(option => fromCheckedState.get(option.value))
-        .map(option => option.value);
-    }
-
-    // eslint-disable-next-line react/no-deprecated, camelcase
-    UNSAFE_componentWillReceiveProps(nextProps) {
-      warning(
-        this.getIsControlled(this.props) === this.getIsControlled(nextProps),
-        '<SelectList> should not switch from controlled to uncontrolled (or vice versa).'
-      );
-
-      if (this.getIsControlled(nextProps)) {
-        this.setState({
-          checkedState: getInitialCheckedState(nextProps.value, nextProps.multiple),
-        });
-      } else if (this.props.multiple !== nextProps.multiple) {
+  // Reset state when multiple prop changes
+  useEffect(
+    () => {
+      if (!isControlled && prevMultiple.current !== multiple) {
         warning(false, '<SelectList>: you should not change `multiple` prop while it is uncontrolled. Its value will be reset now.');
-        this.setState({
-          checkedState: getInitialCheckedState([]),
+        setCheckedState(getInitialCheckedState([]));
+      }
+      // Saving current value to ref for comparing it on next render
+      prevMultiple.current = multiple;
+    },
+    [multiple, isControlled]
+  );
+
+  // Check if the component is switching between controlled and uncontrolled
+  useEffect(() => {
+    warning(
+      isControlled === prevIsControlled.current,
+      '<SelectList> should not switch from controlled to uncontrolled (or vice versa).'
+    );
+
+    // Saving current value for the next comparison
+    prevIsControlled.current = isControlled;
+  }, [isControlled]);
+
+  const handleChange = (nextCheckedState) => {
+    if (!isControlled) {
+      setCheckedState(nextCheckedState);
+    }
+
+    const newValues = options
+      .filter(option => nextCheckedState.get(option.value))
+      .map(option => option.value);
+
+    onChange(multiple ? newValues : newValues[0]);
+  };
+
+  const handleOptionChange = (optionValue, isChecked) => {
+    let nextState = checkedState;
+
+    if (multiple) {
+      nextState = nextState.set(optionValue, isChecked);
+
+      const nextCheckedSize = nextState.filter(_value => _value).size;
+
+      if (nextCheckedSize < minCheck) {
+        // Cancel this operation
+        return;
+      }
+    } else {
+      const currentCheckedOptionValue = checkedState.findKey(_value => _value);
+
+      if (optionValue === currentCheckedOptionValue) {
+        // Does not consider a change
+        return;
+      }
+      nextState = nextState
+        .set(currentCheckedOptionValue, false)
+        .set(optionValue, isChecked);
+    }
+
+    handleChange(nextState);
+  };
+
+  const handleCheckAllOptionChange = (ignoreThis, isChecked) => {
+    const variableOptions = options.filter(option => !option.readOnly);
+
+    const nextState = checkedState.withMutations((map) => {
+      variableOptions.forEach(option => map.set(option.value, isChecked));
+
+      const nextCheckedSize = map.filter(_value => _value).size;
+      const checksNeeded = minCheck - nextCheckedSize;
+
+      // Check options until matching minCheck
+      if (checksNeeded > 0) {
+        variableOptions.slice(0, checksNeeded)
+          .forEach(option => map.set(option.value, true));
+      }
+    });
+
+    handleChange(nextState);
+  };
+
+  const renderOptions = childrenToRender => React.Children.map(
+    childrenToRender,
+    (child) => {
+      const elementTypeSymbol = getElementTypeSymbol(child);
+      if (
+        elementTypeSymbol === CHECKBOX_OPTION_TYPE_SYMBOL
+        || elementTypeSymbol === RADIO_OPTION_TYPE_SYMBOL
+      ) {
+        return React.cloneElement(child, {
+          checked: checkedState.get(child.props.value),
+          onChange: handleOptionChange,
         });
       }
-    }
 
-    handleChange(nextCheckedState) {
-      const { onChange, multiple } = this.props;
-      const nextValues = this.getValues(nextCheckedState);
-
-      if (!this.getIsControlled()) {
-        this.setState({ checkedState: nextCheckedState });
-      }
-      onChange(multiple ? nextValues : nextValues[0]);
-    }
-
-    handleOptionChange = (optionValue, isChecked) => {
-      const { multiple, minCheck } = this.props;
-      const { checkedState } = this.state;
-
-      let nextState = checkedState;
-
-      if (multiple) {
-        nextState = nextState.set(optionValue, isChecked);
-
-        const nextCheckedSize = nextState.filter(value => value).size;
-
-        if (nextCheckedSize < minCheck) {
-          // Cancel this operation
-          return;
-        }
-      } else {
-        const currentCheckedOptionValue = checkedState.findKey(value => value);
-
-        if (optionValue === currentCheckedOptionValue) {
-          // Does not consider a change
-          return;
-        }
-        nextState = nextState
-          .set(currentCheckedOptionValue, false)
-          .set(optionValue, isChecked);
+      if (child && child.type === React.Fragment) {
+        return renderOptions(child.props.children);
       }
 
-      this.handleChange(nextState);
+      return child;
     }
+  );
 
-    handleCheckAllOptionChange = (ignoreThis, isChecked) => {
-      const { minCheck } = this.props;
-      const { checkedState } = this.state;
+  const renderCheckAllOption = () => (
+    <Option
+      label={checkAllLabel}
+      value={null}
+      checked={isAllChecked}
+      onChange={handleCheckAllOptionChange}
+    />
+  );
 
-      const variableOptions = this.getOptions()
-        .filter(option => !option.readOnly);
-
-      const nextState = checkedState.withMutations((map) => {
-        variableOptions.forEach(option => map.set(option.value, isChecked));
-
-        const nextCheckedSize = map.filter(value => value).size;
-        const checksNeeded = minCheck - nextCheckedSize;
-
-        // Check options until matching minCheck
-        if (checksNeeded > 0) {
-          variableOptions.slice(0, checksNeeded)
-            .forEach(option => map.set(option.value, true));
-        }
-      });
-
-      this.handleChange(nextState);
-    }
-
-    renderOptions(children = this.props.children) {
-      const { checkedState } = this.state;
-
-      return React.Children.map(children, (child) => {
-        const elementTypeSymbol = getElementTypeSymbol(child);
-        if (
-          elementTypeSymbol === CHECKBOX_OPTION_TYPE_SYMBOL
-          || elementTypeSymbol === RADIO_OPTION_TYPE_SYMBOL
-        ) {
-          return React.cloneElement(child, {
-            checked: checkedState.get(child.props.value),
-            onChange: this.handleOptionChange,
-          });
-        }
-
-        if (child && child.type === React.Fragment) {
-          return this.renderOptions(child.props.children);
-        }
-
-        return child;
-      });
-    }
-
-    renderCheckAllOption() {
-      const { checkAllLabel } = this.props;
-      const isAllChecked = this.getIsAllChecked();
-
-      return (
-        <Option
-          label={checkAllLabel}
-          value={null}
-          checked={isAllChecked}
-          onChange={this.handleCheckAllOptionChange}
-        />
-      );
-    }
-
-    render() {
-      const {
-        multiple,
-        showCheckAll,
-        title,
-        desc,
-      } = this.props;
-      const wrapperProps = getRemainingProps(this.props, SelectList.propTypes);
-
-      return (
-        <List title={title} desc={desc} {...wrapperProps}>
-          {multiple && showCheckAll && this.renderCheckAllOption()}
-          {this.renderOptions()}
-        </List>
-      );
-    }
-}
+  return (
+    <List title={title} desc={desc} {...wrapperProps}>
+      {multiple && showCheckAll && renderCheckAllOption()}
+      {renderOptions(children)}
+    </List>
+  );
+});
 
 export default SelectList;
+
+SelectList.propTypes = {
+  multiple: PropTypes.bool,
+  showCheckAll: PropTypes.bool,
+  minCheck: PropTypes.number,
+  checkAllLabel: PropTypes.node,
+  value: PropTypes.oneOfType([
+    valueType,
+    PropTypes.arrayOf(valueType),
+  ]),
+  defaultValue: PropTypes.oneOfType([
+    valueType,
+    PropTypes.arrayOf(valueType),
+  ]),
+  onChange: PropTypes.func,
+  title: PropTypes.string,
+  desc: PropTypes.node,
+};
+
+SelectList.defaultProps = {
+  multiple: false,
+  showCheckAll: true,
+  minCheck: 0,
+  checkAllLabel: 'All',
+  value: undefined,
+  defaultValue: undefined,
+  onChange: () => {},
+  title: undefined,
+  desc: undefined,
+};
