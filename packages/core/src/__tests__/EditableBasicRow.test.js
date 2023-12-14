fcd79c09f37fb7b6bdc4dd7cc8c05274851b1b48
DMoon <dmoon.t@gmail.com>
2023-12-14 13:37:54 +0800

    refactor: Refactor EditableBasicRow component to use React hooks

--- packages/core/src/__tests__/EditableBasicRow.test.js
+++ packages/core/src/__tests__/EditableBasicRow.test.js
@@ -1,132 +1,140 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow, mount } from 'enzyme';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import EditableBasicRow, { ROW_INPUT_TAGS } from '../EditableBasicRow';
 
-import EditableBasicRow, { BEM } from '../EditableBasicRow';
+describe('<EditableBasicRow />', () => {
+  let value;
+  let onChange;
+  let onFocus;
+  let onBlur;
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  const element = <EditableBasicRow />;
+  beforeEach(() => {
+    value = 'Test value';
+    onChange = jest.fn();
+    onFocus = jest.fn();
+    onBlur = jest.fn();
+  });
 
-  ReactDOM.render(element, div);
-});
+  it('renders correctly', () => {
+    render(
+      <EditableBasicRow
+        value={value}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+    );
 
-it('renders an <input> or <textarea> inside a <BasicRow>', () => {
-  const wrapper = shallow(<EditableBasicRow />);
+    const input = screen.getByRole('textbox');
+    expect(input).toHaveValue(value);
+  });
 
-  expect(wrapper.find('BasicRow')).toHaveLength(1);
-  expect(wrapper.find('BasicRow').find('input')).toHaveLength(1);
-  expect(wrapper.find('BasicRow').find('textarea')).toHaveLength(0);
+  it('handles input change', () => {
+    render(
+      <EditableBasicRow
+        value={value}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+    );
 
-  wrapper.setProps({ inputTag: 'textarea' });
-  expect(wrapper.find('BasicRow').find('input')).toHaveLength(0);
-  expect(wrapper.find('BasicRow').find('textarea')).toHaveLength(1);
-});
+    const input = screen.getByRole('textbox');
+    userEvent.type(input, 'New test value');
 
-it('updates state on input focus/blur', () => {
-  const wrapper = shallow(<EditableBasicRow />);
-  const inputWrapper = wrapper.find('input');
+    expect(onChange).toHaveBeenCalled();
+  });
 
-  expect(wrapper.state('focused')).toBeFalsy();
+  it('handles input change and input is not controlled', () => {
+    render(
+      <EditableBasicRow
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+    );
 
-  inputWrapper.simulate('focus');
-  expect(wrapper.state('focused')).toBeTruthy();
+    const input = screen.getByRole('textbox');
+    userEvent.type(input, 'New test value');
 
-  inputWrapper.simulate('blur');
-  expect(wrapper.state('focused')).toBeFalsy();
-});
+    expect(screen.getByText('New test value')).toBeInTheDocument();
+  });
 
-it('forwards onFocus/onBlur events', () => {
-  const handleFocus = jest.fn();
-  const handleBlur = jest.fn();
+  it('handles input focus and blur', () => {
+    render(
+      <EditableBasicRow
+        value={value}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+    );
 
-  const wrapper = shallow(<EditableBasicRow onFocus={handleFocus} onBlur={handleBlur} />);
-  const inputWrapper = wrapper.find('input');
+    const input = screen.getByRole('textbox');
+    fireEvent.focus(input);
+    expect(onFocus).toHaveBeenCalled();
 
-  expect(handleFocus).not.toHaveBeenCalled();
-  expect(handleBlur).not.toHaveBeenCalled();
+    fireEvent.blur(input);
+    expect(onBlur).toHaveBeenCalled();
+  });
 
-  inputWrapper.simulate('focus');
-  expect(handleFocus).toHaveBeenCalledTimes(1);
-  expect(handleBlur).not.toHaveBeenCalled();
+  it('renders textarea when inputTag is set to TEXTAREA', () => {
+    render(
+      <EditableBasicRow
+        inputTag={ROW_INPUT_TAGS.TEXTAREA}
+        value={value}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+    );
 
-  inputWrapper.simulate('blur');
-  expect(handleFocus).toHaveBeenCalledTimes(1);
-  expect(handleBlur).toHaveBeenCalledTimes(1);
-});
+    const textarea = screen.getByRole('textbox');
+    expect(textarea.tagName).toBe('TEXTAREA');
+    expect(textarea).toHaveValue(value);
+  });
 
-it('forwards onChange event', () => {
-  const handleChange = jest.fn();
-  const wrapper = shallow(<EditableBasicRow onChange={handleChange} />);
-  const inputWrapper = wrapper.find('input');
+  it('renders with default placeholder when value is not provided', () => {
+    render(
+      <EditableBasicRow
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+    );
 
-  expect(handleChange).not.toHaveBeenCalled();
+    const input = screen.getByPlaceholderText('Unset');
+    expect(input).toHaveValue('');
+  });
 
-  inputWrapper.simulate('change', { currentTarget: document.createElement('input') });
-  expect(handleChange).toHaveBeenCalledTimes(1);
-});
+  it('renders as disabled when disabled prop is true', () => {
+    render(
+      <EditableBasicRow
+        value={value}
+        disabled
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+    );
 
-it('updates user input inside state when not controlled', () => {
-  const wrapper = shallow(<EditableBasicRow defaultValue="Foo" />);
-  const inputWrapper = wrapper.find('input');
-  const mockedInput = document.createElement('input');
+    const input = screen.getByRole('textbox');
+    expect(input).toBeDisabled();
+  });
 
-  expect(wrapper.state('currentValue')).toBe('Foo');
+  it('renders as read-only when readOnly prop is true', () => {
+    render(
+      <EditableBasicRow
+        value={value}
+        readOnly
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+    );
 
-  mockedInput.value = 'Bar';
-  inputWrapper.simulate('change', { currentTarget: mockedInput });
-  expect(wrapper.state('currentValue')).toBe('Bar');
-});
-
-it('freezes input value when controlled via prop', () => {
-  const wrapper = shallow(<EditableBasicRow value="Foo" />);
-  const inputWrapper = wrapper.find('input');
-  const mockedInput = document.createElement('input');
-
-  expect(wrapper.state('currentValue')).toBe('Foo');
-
-  mockedInput.value = 'Bar';
-  inputWrapper.simulate('change', { currentTarget: mockedInput });
-  expect(wrapper.state('currentValue')).toBe('Foo');
-});
-
-it('renders basic label with the same visual text from input', () => {
-  const wrapper = mount(<EditableBasicRow value="Foo" placeholder="Unset" />);
-  const labelClass = `.${BEM.basicLabel}`;
-  expect(wrapper.find(labelClass).text()).toBe('Foo');
-
-  wrapper.setProps({ value: 'Bar' });
-  expect(wrapper.find(labelClass).text()).toBe('Bar');
-
-  wrapper.setProps({ value: '' });
-  expect(wrapper.find(labelClass).text()).toBe('Unset');
-});
-
-it('renders basic label with additional line-break when tag is textarea', () => {
-  const wrapper = mount(
-    <EditableBasicRow inputTag="textarea" value="Foo" placeholder="Unset" />
-  );
-  const labelClass = `.${BEM.basicLabel}`;
-  expect(wrapper.find(labelClass).text()).toBe('Foo\n');
-
-  wrapper.setProps({ value: 'Foo\n' });
-  expect(wrapper.find(labelClass).text()).toBe('Foo\n\n');
-});
-
-it('keeps input from keyboard navigation when input looks untouchable', () => {
-  const wrapper = shallow(<EditableBasicRow readOnly={false} disabled={false} />);
-  expect(wrapper.find('input').prop('tabIndex')).toBeUndefined();
-
-  wrapper.setProps({ readOnly: true, disabled: false });
-  expect(wrapper.find('input').prop('tabIndex')).toBe(-1);
-
-  wrapper.setProps({ readOnly: false, disabled: true });
-  expect(wrapper.find('input').prop('tabIndex')).toBe(-1);
-});
-
-it('passes unknown props to its underlying input', () => {
-  const wrapper = shallow(<EditableBasicRow autoFocus id="foo-input" />);
-
-  expect(wrapper.find('input').prop('autoFocus')).toBeTruthy();
-  expect(wrapper.find('input').prop('id')).toBe('foo-input');
+    const input = screen.getByRole('textbox');
+    expect(input).toHaveAttribute('readonly');
+  });
 });
